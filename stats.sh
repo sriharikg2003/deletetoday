a2c076f4efbc780d671d64b283df7d4446612846
sriharikg2003 <sriharikg2003@gmail.com>
2023-10-09 04:09:06 +0530

    Output Stats

---
+++ stats.sh
@@ -0,0 +1,13 @@
+java -jar jars/simulator.jar src/configuration/config.xml OUTPUT/evenodd.txt test_cases/evenorodd.out
+
+java -jar jars/simulator.jar src/configuration/config.xml OUTPUT/palindrome.txt test_cases/palindrome.out
+
+
+java -jar jars/simulator.jar src/configuration/config.xml OUTPUT/prime.txt test_cases/prime.out
+
+
+java -jar jars/simulator.jar src/configuration/config.xml OUTPUT/descending.txt test_cases/descending.out
+
+
+java -jar jars/simulator.jar src/configuration/config.xml OUTPUT/fibonacci.txt test_cases/fibonacci.out
+
